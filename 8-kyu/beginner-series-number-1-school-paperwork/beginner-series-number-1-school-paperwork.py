eec998ece8361001962c003429ffd84bf3dd446f
LiAdonis11 <127942925+LiAdonis11@users.noreply.github.com>
2026-03-11 21:50:31 +0800

    Add solution - CodeHub

---
+++ 8-kyu/beginner-series-number-1-school-paperwork/beginner-series-number-1-school-paperwork.py
@@ -0,0 +1,10 @@
+def paperwork(n, m):
+    # Happy Coding! ^_^
+    if n < 0 or m < 0:    
+        return 0
+        
+    blank_pages = n * m
+    return blank_pages
+​
+outcome = paperwork(-5,5)
+print(outcome)
